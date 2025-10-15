0610ee6a2ee1f2ea32d42db8ae28cb9d5154d00e
Prakatish Krishnan <vrprakatish@gmail.com>
2025-10-15 00:05:02 -0700

    Initial Commit

---
+++ Glassy/GlassyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassyApp.swift
+//  Glassy
+//
+//  Created by Prakatish Krishnan on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlassyTests/GlassyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlassyTests.swift
+//  GlassyTests
+//
+//  Created by Prakatish Krishnan on 10/15/25.
+//
+
+import Testing
+@testable import Glassy
+
+struct GlassyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
